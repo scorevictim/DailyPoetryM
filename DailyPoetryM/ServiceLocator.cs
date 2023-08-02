25c467918fc1317588d29ff808c3527d3121ea74
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-02 13:18:33 -0700

    add dependency injection container

---
+++ DailyPoetryM/ServiceLocator.cs
@@ -0,0 +1,19 @@
+﻿using DailyPoetryM.Services;
+using DailyPoetryM.ViewModels;
+
+namespace DailyPoetryM;
+
+public class ServiceLocator
+{
+    private IServiceProvider serviceProvider;
+    public ResultPageViewModel ResultPageViewModel => serviceProvider.GetService<ResultPageViewModel>();
+
+    public ServiceLocator()
+    {
+        ServiceCollection serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton<IPoetryStorage, PoetryStorage>();
+        serviceCollection.AddSingleton<ResultPageViewModel>();
+
+        serviceProvider = serviceCollection.BuildServiceProvider();
+    }
+}
