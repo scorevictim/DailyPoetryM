da141eb5e27c3eb98ccf62b6244ee4b2bea36b97
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-02 13:23:13 -0700

    implement IPreferenceStorage

---
+++ DailyPoetryM/Services/PreferenceStorage.cs
@@ -0,0 +1,14 @@
+﻿namespace DailyPoetryM.Services;
+
+public class PreferenceStorage : IPreferenceStorage
+{
+    public int Get(string key, int defaultValue)
+    {
+        return Preferences.Get(key, defaultValue);
+    }
+
+    public void Set(string key, int value)
+    {
+        Preferences.Set(key, value);
+    }
+}
