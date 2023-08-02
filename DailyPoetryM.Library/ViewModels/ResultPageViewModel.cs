57cfca8dde5cf6a9aea9f7fb1b443e8dff0a0a1c
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-02 09:40:21 -0700

    add logic for infinite scrolling

--- DailyPoetryM.Library/ViewModels/ResultPageViewModel.cs
+++ DailyPoetryM.Library/ViewModels/ResultPageViewModel.cs
@@ -1,8 +1,73 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DailyPoetryM.Models;
+using DailyPoetryM.Services;
+using System.Linq.Expressions;
+using TheSalLab.MauiInfiniteScrolling;
 
 namespace DailyPoetryM.ViewModels;
 
 public class ResultPageViewModel : ObservableObject
 {
+    private Expression<Func<Poetry, bool>> where;
+    public Expression<Func<Poetry, bool>> Where
+    {
+        get => where;
+        set
+        {
+            //if(value != where)
+            //{
+            //    canLoadMore = true;
+            //}
+            canLoadMore = SetProperty(ref where, value);
+        }
+    }
+    private string status;
+    public string Status
+    {
+        get => status;
+        set => SetProperty(ref status, value);
+    }
+    public MauiInfiniteScrollCollection<Poetry> Poetries { get; }
 
+    public ResultPageViewModel(IPoetryStorage poetryStorage)
+    {
+        Poetries = new()
+        {
+            OnCanLoadMore = () => canLoadMore,
+            OnLoadMore = async () =>
+            {
+                Status = Loading;
+                var poetries = (await poetryStorage.GetPoetriesAsync(Where,
+                    Poetries.Count, PageSize)).ToList();
+
+                if (poetries.Count < PageSize)
+                {
+                    // NoMoreResult
+                    canLoadMore = false;
+                    Status = NoMoreResult;
+                }
+
+                if (poetries.Count == 0 && Poetries.Count == 0)
+                {
+                    // NoResult
+                    canLoadMore = false;
+                    Status = NoResult;
+                }
+
+                return poetries;
+            }
+        };
+    }
+    private RelayCommand navigatedToCommand;
+    public RelayCommand NavigatedToCommand => navigatedToCommand ??= new(async () =>
+    {
+        Poetries.Clear();
+        await Poetries.LoadMoreAsync();
+    });
+    private bool canLoadMore;
+    public const string Loading = "Loading";
+    public const string NoResult = "No Result";
+    public const string NoMoreResult = "End";
+    public const int PageSize = 20;
 }
