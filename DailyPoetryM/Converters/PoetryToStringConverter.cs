2b76e5f29cf7d1046bf5e3f04b4cd104feac34f1
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-03 18:50:52 -0700

    add converter for Poetry

---
+++ DailyPoetryM/Converters/PoetryToStringConverter.cs
@@ -0,0 +1,17 @@
+﻿using DailyPoetryM.Models;
+using System.Globalization;
+
+namespace DailyPoetryM.Converters;
+
+public class PoetryToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is not Poetry poetry ? throw new Exception() : $"{poetry.Dynasty} {poetry.Author} {poetry.Snippet}";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
