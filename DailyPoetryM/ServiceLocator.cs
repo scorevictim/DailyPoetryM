42c1b9259be0b840eacf15d50d34b87fd23aa35a
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-05 17:41:39 -0700

    add ViewModel to DI

--- DailyPoetryM/ServiceLocator.cs
+++ DailyPoetryM/ServiceLocator.cs
@@ -7,14 +7,19 @@ public class ServiceLocator
 {
     private IServiceProvider serviceProvider;
     public ResultPageViewModel ResultPageViewModel => serviceProvider.GetService<ResultPageViewModel>();
+    public TodayPageViewModel TodayPageViewModel => serviceProvider.GetService<TodayPageViewModel>();
 
     public ServiceLocator()
     {
-        ServiceCollection serviceCollection = new ServiceCollection();
+        ServiceCollection serviceCollection = new();
         serviceCollection.AddSingleton<IPoetryStorage, PoetryStorage>();
-        serviceCollection.AddSingleton<ResultPageViewModel>();
         serviceCollection.AddSingleton<IPreferenceStorage, PreferenceStorage>();
 
+        serviceCollection.AddSingleton<ResultPageViewModel>();
+        serviceCollection.AddSingleton<TodayPageViewModel>();
+
+        serviceCollection.AddSingleton<ITodayPoetryService, TodayPoetryService>();
+        serviceCollection.AddSingleton<IAlertService, AlertService>();
         serviceProvider = serviceCollection.BuildServiceProvider();
     }
 }
