8b289717cb6cdbe4692ef885885ecad8edc9b856
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-05 17:41:20 -0700

    false implement AlertService

---
+++ DailyPoetryM/Services/AlertService.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyPoetryM.Services;
+
+public class AlertService : IAlertService
+{
+    public void Alert(string title, string message, string button)
+    {
+        throw new NotImplementedException();
+    }
+}
