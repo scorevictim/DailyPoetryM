489cb257d432cc6d9ea1a3091393986fdeae804e
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-05 17:15:00 -0700

    add unit test for TodayPoetryService

---
+++ DailyPoetryM.UnitTest/Services/TodayPoetryServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using DailyPoetryM.Services;
+
+namespace DailyPoetryM.UnitTest.Services;
+
+public class TodayPoetryServiceTest
+{
+    [Fact]
+    public async Task GetTodayPoetryAsync_Default()
+    {
+        TodayPoetryService todayPoetryService = new(null, null);
+        var result = await todayPoetryService.GetTodayPoetryAsync();
+        Assert.NotNull(result);
+    }
+}
