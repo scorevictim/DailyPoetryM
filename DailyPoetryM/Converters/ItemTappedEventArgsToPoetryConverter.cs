367dc1a4779cc4c6c28a3dbafdaf380c01108fd6
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-07 08:37:31 -0700

    add converter for event args

---
+++ DailyPoetryM/Converters/ItemTappedEventArgsToPoetryConverter.cs
@@ -0,0 +1,17 @@
+﻿using DailyPoetryM.Models;
+using System.Globalization;
+
+namespace DailyPoetryM.Converters;
+
+public class ItemTappedEventArgsToPoetryConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (value as ItemTappedEventArgs)?.Item as Poetry;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
