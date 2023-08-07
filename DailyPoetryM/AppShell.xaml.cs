d0bdf3bf908a45413d6ccdeb69b4645c2d4c84a0
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-06 21:18:05 -0700

    route change

--- DailyPoetryM/AppShell.xaml.cs
+++ DailyPoetryM/AppShell.xaml.cs
@@ -1,16 +1,20 @@
 ﻿using DailyPoetryM.Pages;
+using DailyPoetryM.Services;
 
 namespace DailyPoetryM;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
+    public AppShell()
+    {
+        InitializeComponent();
+        var serviceLocatorName = nameof(ServiceLocator);
+        var serviceLocator = (ServiceLocator)Application.Current.Resources.MergedDictionaries.First(p => p.ContainsKey(serviceLocatorName))[serviceLocatorName];
+        var routeService = serviceLocator.RouteService;
         Items.Add(new FlyoutItem
         {
             Title = nameof(TodayPage),
-            Route = nameof(TodayPage),
+            Route = routeService.GetRoute(RootNavigationConstant.TodayPage),
             Items =
             {
                 new ShellContent
@@ -19,17 +23,19 @@ public partial class AppShell : Shell
                 }
             }
         });
+        Routing.RegisterRoute(routeService.GetRoute(nameof(RootNavigationConstant.TodayPage)), typeof(TodayDetailPage));
         Items.Add(new FlyoutItem
-		{
-			Title = nameof(ResultPage),
-			Route = nameof(ResultPage),
-			Items =
-			{
-				new ShellContent
-				{
-					ContentTemplate = new(typeof(ResultPage))
-				}
-			}
-		});
-	}
+        {
+            Title = nameof(ResultPage),
+            Route = routeService.GetRoute(nameof(ContentNavigationConstant.ResultPage)),
+            Items =
+            {
+                new ShellContent
+                {
+                    ContentTemplate = new(typeof(ResultPage))
+                }
+            }
+        });
+        Routing.RegisterRoute(routeService.GetRoute(ContentNavigationConstant.DetailPage), typeof(DetailPage));
+    }
 }
