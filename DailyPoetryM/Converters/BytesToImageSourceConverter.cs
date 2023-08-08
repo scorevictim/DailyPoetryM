64b68d915b180c3d8c4ca5afb111b52725b6535c
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-07 19:52:10 -0700

    add converter

---
+++ DailyPoetryM/Converters/BytesToImageSourceConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace DailyPoetryM.Converters;
+
+public class BytesToImageSourceConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is byte[] bytes ? ImageSource.FromStream(() => new MemoryStream(bytes)) : null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
