166e93e7ffcb77f41e6000f5358197c3c0d920e2
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-07 18:15:16 -0700

    add function for datetime

--- DailyPoetryM/Services/PreferenceStorage.cs
+++ DailyPoetryM/Services/PreferenceStorage.cs
@@ -12,12 +12,22 @@ public class PreferenceStorage : IPreferenceStorage
         return Preferences.Get(key, defaultValue);
     }
 
+    public DateTime Get(string key, DateTime defaultValue)
+    {
+        return Preferences.Get(key, defaultValue);
+    }
+
     public void Set(string key, int value)
     {
         Preferences.Set(key, value);
     }
 
     public void Set(string key, string value)
+    {
+        Preferences.Set(key, value);
+    }
+
+    public void Set(string key, DateTime value)
     {
         Preferences.Set(key, value);
     }
