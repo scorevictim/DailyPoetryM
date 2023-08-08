4541a20adde3221b77475b66acc6d7a4e7e3fc90
Scorevictim <30682499+scorevictim@users.noreply.github.com>
2023-08-08 12:46:54 -0700

    add favorite model

---
+++ DailyPoetryM.Library/Models/Favorite.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SQLite;
+
+namespace DailyPoetryM.Models;
+
+public class Favorite : ObservableObject
+{
+    [PrimaryKey]
+    public int PoetryId { get; set; }
+    private bool isFavorite;
+    public virtual bool IsFavorite
+    {
+        get => isFavorite;
+        set => SetProperty(ref isFavorite, value);
+    }
+    public long TimeStamp { get; set; }
+}
